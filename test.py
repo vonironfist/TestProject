146b67d319ff6bf1eae289c6e36b4cab40a02d94
vonironfist <Tianhui.yang@outlook.com>
2017-06-09 03:06:49 +0800

    nn

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-# Here's some new strange stuff, remember type it exactly.
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
 
-days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
 
-print ('Here are the days:'), days
-print ('Here are the months:'), months
-
-print ("""
-There's something going on here.
-With the three double-quotes.
-We'll be able to type as much as we like.
-Even 4 lines if we want, or 5, or 6.
-""")
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
